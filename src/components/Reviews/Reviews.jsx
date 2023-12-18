e1fa9c0afac63ca2d6032f6bc5cbdefccb70975f
MateuszKar2 <system505@protonmail.com>
2023-12-18 23:52:22 +0100

    add file

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -15,6 +15,32 @@ const Reviews = () => {
             } catch (error) {
                 console.log(error);
             }
-        }
-    })
-}
+        };
+
+        fetchReviews();
+    }, [movieId]);
+
+    return(
+        <Wrapper>
+            <RewievHeader>
+                Reviews
+            </RewievHeader>
+            {reviews.length ? (
+                <RewievList className="reviews-container">
+                    {reviews.map(review => (
+                        <RewievListItem className="review-card" key={review.id}>
+                           <Author>Author: {review.author}</Author> 
+                           <Rewiev>{review.content}</Rewiev>
+                        </RewievListItem>
+                    ))}
+                </RewievList>
+            ) : (
+                <NoReviewsText>
+                    We don't have any rewievs for this movie yet
+                </NoReviewsText>
+            )}
+        </Wrapper>
+    );
+};
+
+export default Reviews;
