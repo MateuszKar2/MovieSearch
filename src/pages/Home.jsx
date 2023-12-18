fe94e720844ba8bde0effb6ec8e4b43457639c15
MateuszKar2 <system505@protonmail.com>
2023-12-18 22:17:29 +0100

    add file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+const { useState, useEffect } = require("react");
+import { fetchTrendMovies } from "services/api";
+import MovieList from "components/MovieList/MovieList";
+import  { LoadingIndicator } from "./SharedLayout.styled";
+
+
+
+
+const Home = () => {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        const fetchTrendingMovies = async () => {
+            try {
+                setError(false);
+                setIsLoading(true);
+                const data = await fetchTrendMovies();
+                setTrendingMovies(data.results);               
+            } catch (error) {
+                setError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchTrendingMovies();
+    }, []);
+
+    return (
+        <>
+        {isLoading ? (
+            <LoadingIndicator/>
+        ) : error ? (
+            <p>
+                Sorry, we could not fetch the trending movies. Please try again later.
+            </p>
+        ) : (
+            <MovieList trendingMovies={trendingMovies || []} />
+        )}
+        </>
+    );
+};
+
+export default Home;
