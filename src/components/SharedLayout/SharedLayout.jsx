7d638464bc2ebb34f5d48d40ceacc3ba4adf5abd
MateuszKar2 <system505@protonmail.com>
2023-12-22 21:52:32 +0100

    add file

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,23 +1,24 @@
-import React, {Suspense} from "react";
+
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { LoadingIndicator } from './LoadingDots';
-import { StyledHeader, StyledNavlink } from "./SharedLayout.styled";
+import { StyledHeader, StyledNavLink } from './SharedLayout.styled';
 
 const SharedLayout = () => {
-    return (
-        <>
-            <StyledHeader>
-                <nav>
-                    <StyledNavlink to="/">Home</StyledNavlink>
-                    <StyledNavlink to="/movies">Movies</StyledNavlink>
-                </nav>
-            </StyledHeader>
+  return (
+    <>
+      <StyledHeader>
+        <nav>
+          <StyledNavLink to="/">Home</StyledNavLink>
+          <StyledNavLink to="/movies">Movies</StyledNavLink>
+        </nav>
+      </StyledHeader>
 
-            <Suspense fallback={<LoadingIndicator />}>
-                <Outlet />
-            </Suspense>
-        </>
-    );
+      <Suspense fallback={<LoadingIndicator />}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
 };
 
 export default SharedLayout;
