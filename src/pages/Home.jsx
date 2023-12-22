2c0b47d16ea177b261174c8607ca25b3f4a7a810
MateuszKar2 <system505@protonmail.com>
2023-12-22 21:52:42 +0100

    add file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,44 @@
-const { useState, useEffect } = require("react");
-import { fetchTrendMovies } from "services/api";
-import MovieList from "components/MovieList/MovieList";
-import  { LoadingIndicator } from '../components/SharedLayout/LoadingDots';
 
-
-
+import { useEffect, useState } from 'react';
+import { fetchTrendMovies } from '../services/api';
+import MovieList from '../components/MovieList/MovieList';
+import { LoadingIndicator } from '../components/SharedLayout/LoadingDots';
 
 const Home = () => {
-    const [trendingMovies, setTrendingMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        setError(false);
+        setIsLoading(true);
+        const data = await fetchTrendMovies();
+        setTrendingMovies(data.results);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        const fetchTrendingMovies = async () => {
-            try {
-                setError(false);
-                setIsLoading(true);
-                const data = await fetchTrendMovies();
-                setTrendingMovies(data.results);               
-            } catch (error) {
-                setError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        };
+    fetchTrendingMovies();
+  }, []);
 
-        fetchTrendingMovies();
-    }, []);
-
-    return (
-        <>
-        {isLoading ? (
-            <LoadingIndicator/>
-        ) : error ? (
-            <p>
-                Sorry, we could not fetch the trending movies. Please try again later.
-            </p>
-        ) : (
-            <MovieList trendingMovies={trendingMovies || []} />
-        )}
-        </>
-    );
+  return (
+    <>
+      {isLoading ? (
+        <LoadingIndicator />
+      ) : error ? (
+        <p>
+          Sorry, we could not fetch the trending movies. Please try again later.
+        </p>
+      ) : (
+        <MovieList trendingMovies={trendingMovies || []} />
+      )}
+    </>
+  );
 };
 
 export default Home;
